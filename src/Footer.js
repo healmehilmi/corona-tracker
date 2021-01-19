27d45e48949301584a0c36a53fdf4350405c9e53
hilmi naz <healmehilmi@gmail.com>
2021-01-19 09:35:32 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,37 @@
+
+import React from 'react'
+import "./Footer.css";
+
+import logo from "./cogo.png"
+
+
+function Footer() {
+    return (
+        <footer className="Footer">
+            
+
+
+               
+
+              
+                  <ul className="footer_info">
+                    
+                    <li><h2 className="footer_h2">Information</h2></li>
+                    <li><p className="footer_p">This website made with Love and ReactJS.</p></li>
+                    <li><p className="footer_p"> Contact:healmehilmi@gmail.com </p></li>
+                    <li><p className="footer_p"> logo / development © 2021 by Hilmi Naz </p></li>
+
+                </ul>
+          
+          
+          
+           
+         
+           
+        </footer>
+    
+    )
+}
+
+export default Footer
+
